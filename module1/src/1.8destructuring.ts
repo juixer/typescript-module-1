d30af51234ef70ebe6832a891860ea2e3886c5af
Ashfakul Islam <r4pido3@gmail.com>
2024-05-01 00:10:02 +0600

    1.8 destructuring

---
+++ module1/src/1.8destructuring.ts
@@ -0,0 +1,32 @@
+{
+
+    // Object Destructuring
+
+    const user : {
+        name : {
+            firstName : string;
+            middleName : string;
+            lastName : string;
+        };
+        contact : number;
+        address : string;
+    } = {
+        name : {
+            firstName : 'Ashfakul',
+            middleName : 'Islam',
+            lastName : 'Shanto'
+        },
+        contact : 1234567890,
+        address : 'Dhaka'
+    }
+
+
+    const {contact, name: { middleName }} = user
+
+
+    // Array Destructuring
+
+    const friends : string[] = ['Sazin', 'Raadin', 'Sifat', 'Mahin']
+
+    const [a,homie,...rest] = friends
+}
